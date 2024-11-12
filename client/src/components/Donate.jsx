2bc1b51443bf44628b8c200e1e10685c55c7e0d1
DevBhojani116 <devbhojani116@gmail.com>
2024-11-13 03:31:01 +0530

    donate facility added

--- client/src/components/Donate.jsx
+++ client/src/components/Donate.jsx
@@ -7,34 +7,45 @@ import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 import "../App.css";
 
 const Donate = (props) => {
-
-    const {contract}=props.state;
-    console.log(contract);
-    console.log(props.campaignNumber);
-    const donate = async(event)=>{
-        try {
-            console.log(props.campaignNumber);
-            const amount = { value: ethers.utils.parseEther("0.001","ether") };
-            alert("kuch toh kiya")
-            const transaction = await contract.donate(props.campaignNumber, amount);
-            await transaction.wait();
-            alert("Campaign Donation is successful");
-            window.location.reload();
-          } catch (error) {
-            alert("Donation failed:", error);
-            alert(`Donation failed: ${error.message}`);
-          }
-      }
-    return(
-        <>
-            <form onSubmit={donate}>
-                <div>
-                    <input type="submit" value="Donate to Campaign"  disabled={!contract}/>
-                </div>
-            </form>
-        </>
-    )
-
-}
+  const { contract } = props.state;
+  console.log(contract);
+  console.log(props.campaignNumber);
+  const donate = async (event) => {
+    try {
+      console.log(props.campaignNumber);
+      const money = document.querySelector("#money").value;
+      const amount = { value: ethers.parseEther(money) };
+    //   alert("kuch toh kiya 1");
+      const transaction = await contract.donate(
+        Number(props.campaignNumber),
+        amount
+      );
+    //   alert("kuch toh kiya 2");
+      await transaction.wait();
+      alert("Campaign Donation is successful");
+      window.location.reload();
+    } catch (error) {
+      alert("Donation failed:", error);
+      alert(`Donation failed: ${error.message}`);
+    }
+  };
+  return (
+    <>
+      <form onSubmit={donate}>
+        <div className="inputbox">
+          <input type="text" required="required" id="money" />
+          <span>Amount to donate (in Wei)</span>
+        </div>
+        <div>
+          <input
+            type="submit"
+            value="Donate to Campaign"
+            disabled={!contract}
+          />
+        </div>
+      </form>
+    </>
+  );
+};
 
 export default Donate;
